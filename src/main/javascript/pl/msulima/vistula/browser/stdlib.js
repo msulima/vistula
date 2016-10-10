fa056a2016bb59c327612886d0cd6617a05a5ac2
Mateusz Sulima <mateusz.sulima@allegrogroup.com>
2016-10-10 22:31:36 +0200

    Move dom to vistula2

--- src/main/javascript/pl/msulima/vistula/browser/stdlib.js
+++ src/main/javascript/pl/msulima/vistula/browser/stdlib.js
@@ -1,22 +1,7 @@
 "use strict";
 
-function appendChild(Target, Observables) {
-    let currentChildren = [];
-
-    return vistula.zip([Target, Observables]).rxMap(function ($args) {
-        const target = document.getElementById($args[0]);
-        const nextChildren = $args[1];
-        vistula.dom.updateChildren(target, 0, currentChildren, nextChildren);
-        currentChildren = nextChildren;
-    });
-}
-
 const stdlib = vistula.toObservable({
-    dom: {
-        appendChild: appendChild
-    },
     storage: require("./storage"),
-    appendChild: appendChild,
 });
 
 // FIXME
